572c07117f9f8520c4b0980583287d5a84916a48
Dickson anyaele <dicksonanyaele1234@gmail.com>
2024-06-28 09:28:19 +0000

    proxy working

--- week1/reflectionsXAnnotations/logger/LogExecutionTimeTestMain.java
+++ week1/reflectionsXAnnotations/logger/LogExecutionTimeTestMain.java
@@ -3,12 +3,14 @@ package week1.reflectionsXAnnotations.logger;
 public class LogExecutionTimeTestMain {
 
   public static void main(String[] args) throws InterruptedException {
-    LogInterfaceClass logInterfaceClass = new LogInterfaceClass();
+    LogInterface logInterfaceClass = new LogInterfaceClass();
 
     // creating the proxy for the close to add logger invocations
-    LogInterfaceClass proxy = ProxyFactory.createProxy(logInterfaceClass, LogInterfaceClass.class);
+    LogInterface proxy = ProxyFactory.createProxy(logInterfaceClass,
+        LogInterface.class);
 
     proxy.tryLogger();
+    System.out.println("test");
   }
 
 }
--- week1/reflectionsXAnnotations/logger/ProxyFactory.java
+++ week1/reflectionsXAnnotations/logger/ProxyFactory.java
@@ -9,8 +9,11 @@ import java.lang.reflect.Proxy;
  **/
 public class ProxyFactory {
   @SuppressWarnings("unchecked")
-  public static <T> T createProxy(T target, Class<T> interfaceType) {
-    return (T) Proxy.newProxyInstance(interfaceType.getClassLoader(), new Class<?>[] { interfaceType },
+  public static <T> T createProxy(T target, Class<?> interfaceType) {
+    return (T) Proxy.newProxyInstance(
+        interfaceType.getClassLoader(),
+        new Class<?>[] { interfaceType },
         new LogExecutionTimeHandler(target));
   }
+
 }
